0f807cb7f95c16c94bc0b07aadf60ea5646f1d11
gavhalebhushan <gavhalebhushan95@gmail.com>
2022-03-02 15:57:26 +0530

    End of section 4

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public ITokenService _tokenService { get; }
+        public AccountController( DataContext context, ITokenService tokenService)
+        {
+            _tokenService = tokenService;
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDto>> Register( RegisterDto register )
+        {
+            if( await ValidUsername(register.Username)) return BadRequest("Username already taken");
+
+            using var hmac = new HMACSHA512();
+
+            var user = new AppUser
+            {
+                UserName = register.Username,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(register.Password)),
+                PasswordSalt = hmac.Key
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return new UserDto()
+            {
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login( LoginDto login )
+        {
+            AppUser user = await _context.Users.
+                                SingleOrDefaultAsync( x => x.UserName == login.Username );
+            if( user == null ) return BadRequest("Invalid Username");
+
+            var hmac = new HMACSHA512( user.PasswordSalt );
+
+            var computedhash = hmac.ComputeHash( Encoding.UTF8.GetBytes(login.Password));
+            
+            for( int i = 0; i<computedhash.Length; i++ )
+            {
+                if( computedhash[i] != user.PasswordHash[i] ) return BadRequest("Invalid Password");
+            }
+
+            return new UserDto()
+            {
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
+
+        }
+
+        private async Task<bool> ValidUsername(string username) 
+        {
+            return await _context.Users.AnyAsync( x => x.UserName == username.ToLower());
+        }
+
+    }
+}
---
+++ API/Extensions/ApplicationServiceExtentions.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtentions
+    {
+        public static IServiceCollection AddApplicationService( this IServiceCollection services, 
+                                                                        IConfiguration config )
+        {
+            services.AddScoped<ITokenService, TokenService>();
+           
+            services.AddDbContext<DataContext>( options => 
+            {
+                options.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+            
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+            });
+            
+            return services;
+        }
+    }
+}
